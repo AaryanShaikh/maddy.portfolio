0d0061310e3841820bbccf6d02fa1ec26e6eb9bc
Aaryan Shaikh <aayanshaikh27638@gmail.com>
2023-05-23 15:15:00 +0530

    Update Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,10 @@
-import { Typography } from 'antd'
+import { Typography, message } from 'antd'
 import React from 'react'
 
 const Header = ({ pageSel }) => {
     return (
         <div style={{ height: "66px", display: "flex", alignItems: "center", width: "100%", justifyContent: "space-around" }}>
-            <Typography.Link href="/" style={{ fontFamily: "Avenir LT Std", fontStyle: "normal", fontWeight: "400", fontSize: "16px", lineHeight: "19px", color: "#AFAFAF" }}>Vinod Raichur</Typography.Link>
+            <Typography.Link href="/" onClick={()=>{message.info("version 1.2.1")}} style={{ fontFamily: "Avenir LT Std", fontStyle: "normal", fontWeight: "400", fontSize: "16px", lineHeight: "19px", color: "#AFAFAF" }}>Vinod Raichur</Typography.Link>
             <div style={{ display: "flex", gap: "50px" }}>
                 <Typography.Text style={{ fontFamily: "Avenir LT Std", fontStyle: "normal", fontWeight: "400", fontSize: "16px", lineHeight: "25px", color: pageSel == "Main" ? "#302E2E" : "#AFAFAF" }}>Projects</Typography.Text>
                 <Typography.Text style={{ fontFamily: "Avenir LT Std", fontStyle: "normal", fontWeight: "400", fontSize: "16px", lineHeight: "25px", color: pageSel == "about" ? "#302E2E" : "#AFAFAF" }}>About</Typography.Text>
